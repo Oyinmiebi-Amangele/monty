4ded02c555073d3015a194cc3931961382b39c67
Oyinmiebi Amangele <miebi.amangele@gmail.com>
2023-08-25 02:46:52 +0000

    [feature]:added the add and nop function

--- opcode_instruction2.c
+++ opcode_instruction2.c
@@ -30,3 +30,48 @@ void f_swap(stack_t **head, unsigned int counter)
 	h->n = h->next->n;
 	h->next->n = ax;
 }
+
+/**
+ * f_add - adds the top two elements of tthe stack.
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+ */
+
+void f_add(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	int len = 0, ax;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", counter);
+		fclose(glob.fdis);
+		free(glob.content);
+		free_list(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	ax = h->n + h->next->n;
+	h->next->n = ax;
+	*head = h->next;
+	free(h);
+}
+
+/**
+ * f_nop - nothing
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
