6e5893d27e8180b86d0d18e677e621972a0ff7b9
Oyinmiebi Amangele <miebi.amangele@gmail.com>
2023-08-25 01:21:34 +0000

    fix

--- double_link_func.c
+++ double_link_func.c
@@ -1,84 +1,90 @@
 #include "monty.h"
-/**
- * add_queue - Adds a new node at the end of a queue
- * @head: Pointer to a pointer to the head node of the queue
- * @n: Value to be added to the new node
- *
- * Return: Address of the newly added node, or NULL on failure
- */
 
+/**
+ *add_queue - add a note at the end of the doubly link list
+ *@head: first position of linked list
+ *@n: data to store
+ *Return: a doubly linked list
+ */
 stack_t *add_queue(stack_t **head, const int n)
 {
-	stack_t *new_node = malloc(sizeof(stack_t));
-	stack_t *current;
+	stack_t *temp, *aux;
 
-	if (!new_node)
+	if (head == NULL)
 		return (NULL);
-
-	new_node->n = n;
-	new_node->prev = NULL;
-	new_node->next = NULL;
-
-	current = *head;
-
+	temp = malloc(sizeof(stack_t));
+	if (!temp)
+	{
+		dprintf(2, "Error: malloc failed\n");
+		free_glob();
+		exit(EXIT_FAILURE);
+	}
+	temp->n = n;
+	/*Careful with the first time*/
 	if (*head == NULL)
 	{
-		*head = new_node;
-		return (new_node);
+		temp->next = *head;
+		temp->prev = NULL;
+		*head = temp;
+		return (*head);
 	}
-
-
-	while (current->next != NULL)
-	{
-		current = current->next;
-	}
-
-	current->next = new_node;
-	new_node->prev = current;
-	return (new_node);
+	aux = *head;
+	while (aux->next)
+		aux = aux->next;
+	temp->next = aux->next;
+	temp->prev = aux;
+	aux->next = temp;
+	return (aux->next);
 }
 
 /**
- * add_node - Adds a new node at the beginning of a linked list
- * @head: Pointer to a pointer to the head node of the linked list
- * @n: Value to e added to the new node.
- *
- * Return: Address of the newly added node, or NULL on failure
+ *add_node - add a note at the begining of the doubly link list
+ *@head: first position of linked list
+ *@n: data to store
+ *Return: a doubly linked list
  */
 stack_t *add_node(stack_t **head, const int n)
 {
-	stack_t *new_node = malloc(sizeof(stack_t));
+	stack_t *temp;
 
-	if (!new_node)
+	if (head == NULL)
 		return (NULL);
-
-	new_node->n = n;
-	new_node->prev = NULL;
-	new_node->next = *head;
-
-	if (*head != NULL)
+	temp = malloc(sizeof(stack_t));
+	if (!temp)
 	{
-		(*head)->prev = new_node;
+		dprintf(2, "Error: malloc failed\n");
+		free_glob();
+		exit(EXIT_FAILURE);
 	}
-	*head = new_node;
-	return (new_node);
+	temp->n = n;
+	/*Careful with the first time*/
+	if (*head == NULL)
+	{
+		temp->next = *head;
+		temp->prev = NULL;
+		*head = temp;
+		return (*head);
+	}
+	(*head)->prev = temp;
+	temp->next = (*head);
+	temp->prev = NULL;
+	*head = temp;
+	return (*head);
 }
 
 /**
- * free_list - Frees all nodes in a linked list
- * @head: pointer to the head node of the linked list
+ * free_dlistint - frees the doubly linked list
  *
- * Description: Frees each node in the linked list and sets the head
- * to NULL
+ * @head: head of the list
+ * Return: no return
  */
 void free_list(stack_t *head)
 {
-	while (head != NULL)
-	{
-		stack_t *next_node = head->next;
+	stack_t *tmp;
 
-		free(head);
-		head = next_node;
+	while ((tmp = head) != NULL)
+	{
+		head = head->next;
+		free(tmp);
 	}
 }
-
