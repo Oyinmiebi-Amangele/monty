19cd2f8c8fcedc64ef21e09d8ab2d8aea4676971
Oyinmiebi Amangele <miebi.amangele@gmail.com>
2023-08-25 01:12:18 +0000

    [fix]: changed the function of the opcode

--- opcode_instruction.c
+++ opcode_instruction.c
@@ -1,68 +1,61 @@
 #include "monty.h"
 
 /**
- * f_push - add node to the stack
- * @head: stack head
- * @number: line_number
+ * f_push - pushes an element to the stack
+ *
+ * @head: head of the linked list
+ * @number: line number
  * Return: no return
  */
-
 void f_push(stack_t **head, unsigned int number)
 {
-	int n, j = 0, flag = 0;
+	int n, j;
 
-	if (glob.arg)
+	if (!glob.arg)
 	{
-		if (glob.arg[0] == '-')
-			j++;
-		for (; glob.arg[j] != '\0'; j++)
+		dprintf(2, "L%u: ", number);
+		dprintf(2, "usage: push integer\n");
+		free_glob();
+		exit(EXIT_FAILURE);
+	}
+
+	for (j = 0; glob.arg[j] != '\0'; j++)
+	{
+		if (!isdigit(glob.arg[j]) && glob.arg[j] != '-')
 		{
-			if (glob.arg[j] > 57 || glob.arg[j] < 48)
-				flag = 1;
-		}
-		if (flag == 1)
-		{
-			fprintf(stderr, "L%d: usage: push integer\n", number);
-			fclose(glob.fdis);
-			free(glob.content);
-			free_list(*head);
+			dprintf(2, "L%u: ", number);
+			dprintf(2, "usage: push integer\n");
+			free_glob();
 			exit(EXIT_FAILURE);
 		}
 	}
-	else
-	{
-		fprintf(stderr, "L%d: usage: push integer\n", number);
-		fclose(glob.fdis);
-		free(glob.content);
-		free_list(*head);
-		exit(EXIT_FAILURE);
-	}
+
 	n = atoi(glob.arg);
-	if (glob.lifo == 0)
+
+	if (glob.lifo == 1)
 		add_node(head, n);
 	else
 		add_queue(head, n);
 }
 
 /**
- * f_pall - print the stack
- * @head: stack head
- * @number: not used
+ * f_pall - prints all values on the stack
+ *
+ * @head: head of the linked list
+ * @number: line numbers
  * Return: no return
  */
-
 void f_pall(stack_t **head, unsigned int number)
 {
-	stack_t *h;
-	(void)head;
+	stack_t *aux;
 	(void)number;
 
-	h = *head;
-	if (h == NULL)
-		return;
-	while (h)
+	aux = *head;
+
+	while (aux)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
+		printf("%d\n", aux->n);
+		aux = aux->next;
 	}
 }
+
