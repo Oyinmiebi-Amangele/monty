764f98bbeacd28a779e2fdddfd4592e2e83fa061
wattup4rs <gideonwatt@gmail.com>
2023-08-28 03:27:44 -0700

    fix

--- opcode_instruction4.c
+++ opcode_instruction4.c
@@ -51,37 +51,3 @@ void f_queue(stack_t **head, unsigned int counter)
 	glob.lifo = 1;
 }
 
-/**
- * addqueue - add node to the tail stack
- * @n: new_value
- * @head: head of the stack
- * Return: no return
- */
-void addqueue(stack_t **head, int n)
-{
-	stack_t *new_node, *h;
-	h = *head;
-	new_node = malloc(sizeof(stack_t));
-	if (new_node == NULL)
-	{
-		printf("Error\n");
-	}
-	new_node->n = n;
-	new_node->next = NULL;
-	if (h)
-	{
-		while (h->next)
-			h = h->next;
-	}
-	if (!h)
-	{
-		*head = new_node;
-		new_node->prev = NULL;
-	}
-	else
-	{
-		h->next = new_node;
-		new_node->prev = h;
-	}
-}
-
